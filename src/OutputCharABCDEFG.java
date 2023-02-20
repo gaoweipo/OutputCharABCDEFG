095108f6351cd3d4fe0e2fe062b07f21d50f9672
gaoweipo <gaoweipo@hotmail.com>
2023-02-20 16:04:09 -0500

    print ABCDEFG

---
+++ src/OutputCharABCDEFG.java
@@ -0,0 +1,12 @@
+public class OutputCharABCDEFG {
+    public static void main(String[] args) {
+        char a= '\101';
+        char b = '\102';
+        char c = '\103';
+        char d = '\104';
+        char e = '\105';
+        char f = '\106';
+        char g = '\107';
+        System.out.println("Résultats:"+a + b + c + d + e + f + g);
+    }
+}
